5f773ee4737cf16c20190d3d2b21239ed85ecd9b
Avondeil <123551851+Avondeil@users.noreply.github.com>
2024-02-17 15:41:55 +0500

    Add files via upload

---
+++ Prakt3/Prakt3.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include <iostream>
+#define t 1
+#define z t+2
+#define y 4
+#define x 8*z/(exp(t)+2)-pow(y,2)
+using namespace std;
+int main()
+{
+    //Задание 1
+    cout << "X = " << x << endl;
+    return 0;
+}
+
+
---
+++ Prakt3_2/Prakt3_2.cpp
@@ -0,0 +1,10 @@
+﻿
+#include <iostream>
+#define x
+#define y
+#define z (sin(x/y)/(2*pow(x,2))
+#define b log
+int main()
+{
+    std::cout << "Hello World!\n";
+}
